64c768a4a04ee433a4f841062e660d70ebd2dcb3
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-20 14:07:55 -0500

    added user input for weight and planet

--- Topic 5 assignment part one/Program.cs
+++ Topic 5 assignment part one/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variables
+            double earthWeight, planetWeight;
+            string visitingPlanet;
+
+            Console.WriteLine("Please enter your current earth weight: ");
+            double.TryParse(Console.ReadLine(), out earthWeight);
+            Console.WriteLine(" ");
+            Console.WriteLine("I have information for the following planets:");
+            Console.WriteLine("1. Venus");
+            Console.WriteLine("2. Mars");
+            Console.WriteLine("3. Jupiter");
+            Console.WriteLine("4. Saturn");
+            Console.WriteLine("5. Uranus");
+            Console.WriteLine("6. Neptune");
+            Console.WriteLine("Please enter the name of the planet you are visiting");
+            visitingPlanet = Console.ReadLine();
+            
+
         }
     }
 }
